ea6bff66c52ad5dbcf8335787a2f9f29f08c9a65
Oliver Tworkowski <olitworkowski@gmail.com>
2020-03-30 01:20:19 +0200

    rename and remove unused code

--- covid19_webscrape.py
+++ covid19_webscrape.py
@@ -18,16 +18,11 @@ if db[col_name].count_documents({'_id': str(today)}) == 0:
     site = requests.get('https://www.worldometers.info/coronavirus/').text
     html_parsed = BeautifulSoup(site, 'lxml')
     table = html_parsed.find('table', id="main_table_countries_today")
-    # table_heading = str(table.thead.text).rstrip()
-    '''table_heading = ['Country, Other', 'Total Cases', 'New Cases', 'Total Deaths', 'New Deaths', 'Total Recovered',
-                     'Active Cases', 'Serious, Critical', 'Total Cases/ 1M Population', 'Deaths/ 1M Population',
-                     'First Case']'''
     table_heading = ['country', 'totalCases', 'newCases', 'totalDeaths', 'newDeaths', 'totalRecovered',
                      'activeCases', 'criticalCases', 'totalCases_for1M', 'totalDeaths_for1M',
                      'firstCase']
     table_body = table.tbody
     content = []
-    # data = []
     rows = table_body.find_all('tr')
     for row in rows:
         entry = {}
@@ -50,35 +45,6 @@ if db[col_name].count_documents({'_id': str(today)}) == 0:
             entry[table_heading[i]] = cur_value
         print(f'got data for {entry[table_heading[0]]} successfully')
         content.append(entry)
-        # data.append([ele for ele in cols])
-
-    '''table_content = str(table.tbody.text).replace(',', '').replace('+', '').splitlines()
-    table_content.pop(0)
-    content = []
-
-    index = 1
-    while index < len(table_content):
-        row = {}
-        for col in table_heading:
-            row[col] = table_content[index]
-            if col == 'Deaths/ 1M Population':
-                # skip empty field after this column
-                index += 1
-            index += 1
-        # Error Handling
-        try:
-            int(row[table_heading[0]])
-        except ValueError:
-            print(f'got data for {row[table_heading[0]]} successfully')
-        else:
-            print(f'FAILED to get data for {row}')
-            break
-
-        content.append(row)
-        # skip empty fields
-        index += 2
-    
-    '''
 
     # now = datetime.datetime.now()
     # save_file_name = f'{now.date()}T{now.hour}-{now.minute}_covid_dump.json'
